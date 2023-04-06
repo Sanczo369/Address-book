7895344208be7985c4a1003fed28cb5eda612888
Arek Sanecki <areksanecki@gmail.com>
2023-04-06 20:13:06 +0200

    add elements

--- main.py
+++ main.py
@@ -11,6 +11,41 @@ def main():
     fileMenu = Menu(mainMenu)
     mainMenu.add_cascade(label="File", menu=fileMenu)
 
+    # Add elements
+    name_label = Label(root, text="Imie")
+    name_entry = Entry(root)
+    second_name_label = Label(root, text="Drugie Imie")
+    second_name_entry = Entry(root)
+    surname_label = Label(root, text="Nazwisko")
+    surname_entry = Entry(root)
+    street_label = Label(root, text="Ulica")
+    street_entry = Entry(root)
+    home_number_label = Label(root, text="Numer Mieszkania")
+    home_number_entry = Entry(root)
+    city_label = Label(root, text="Miasto")
+    city_entry = Entry(root)
+    zipcode_label = Label(root, text="Kod Pocztowy")
+    zipcode_entry = Entry(root)
+    adress_email_label = Label(root, text="Adres Email")
+    adress_email_entry = Entry(root)
+    phone_number_label = Label(root, text="Numer Telefonu")
+    phone_number_entry = (root)
+
+    submit_btn=Button(root, text="Submit")
+    clear_btn=Button(root, text="Clear")
+
+
+
+
+
+
+
+
+
+
+
+
+
     root.mainloop()
 if __name__ == '__main__':
     main()
