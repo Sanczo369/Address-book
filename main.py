6f22b95a6b4f69d8851830dd9e02752cd0883190
Arek Sanecki <areksanecki@gmail.com>
2023-04-06 19:57:17 +0200

    add root

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from tkinter import *
 def main():
-    pass
+    # root Option
+    root = Tk()
+    root.title("Address Book")
+    root.iconbitmap("icona.ico")
 
+    # Window Menu
+    mainMenu = Menu()
+    root.config(menu=mainMenu)
+    fileMenu = Menu(mainMenu)
+    mainMenu.add_cascade(label="File", menu=fileMenu)
 
+    root.mainloop()
 if __name__ == '__main__':
     main()
